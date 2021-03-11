491023777614814a37303e6a9e44b4e51e5d9a27
Bob.C <bob.cocksedge@gmail.com>
2021-03-11 15:22:59 +0000

    Complete revision of Random canvas circles and Building number input functions. Next Test your skills: Functions - but not today

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # mdn_return
 "Folder mdn_return opened for MDN exercise return values Initial commit files index.html jscript.js style.CSS then copying exercise file from MDN git and transferring jscript and CSS code to relevant files and linking files to index.html for this commit"
+"Complete first return value exercise Next look at and try subsequent return value execises"
+I have found mdn -Function Return Values chapter so will read and do those exerises.
+"Complete revision of Random canvas circles and backt to building number input functions "
--- jscript.js
+++ jscript.js
@@ -1,3 +1,5 @@
+
+//number input
 const input = document.querySelector('.numberInput');
 const para = document.querySelector('p');
 
@@ -7,7 +9,7 @@ function squared(num) {
     function cubed(num) {
         return num * num * num;
 }
-    function factorial(num) {
+     function factorial(num) {
         if(num <  0) return undefined;
         if(num == 0) return 1;
         let x= num -1;
@@ -15,16 +17,48 @@ function squared(num) {
             num *= x;
             x--;
         }
-        return num;
-    }
+         return num;
+      }
 
 input.onchange= function(){
     const num= input.value;
     if (isNaN(num)) {
         para.textContent= "You'll need to enter a number!";    
     }else{
-        para.textContent= ' ' + num + ' squared is ' + squared(num) + '.' +
-        ' ' + num + ' cubed is ' + cubed (num) + '.' +
-        ' ' + num + ' factorial is ' + factorial(num) + '.';
+        para.textContent= '  ' + num + ' squared = ' + squared(num) + '.' +
+        '  ' + num + ' cubed = ' + cubed (num) + '.' +
+        '  ' + num + ' factorial = ' + factorial(num) + '.';
     }
 }
+
+//What are return values?
+// let myText= "The weather is brightening up!";
+// let newString= myText.replace("brightening up!", "dulling down.");  //replace takes a string function
+// console.log(newString);     //newString is the returned value when function completes running 
+
+//Random canvas circles
+// const btn= document.querySelector('button');
+// const canvas= document.querySelector('canvas');
+// const ctx= canvas.getContext('2d');
+
+// let WIDTH= document.documentElement.clientWidth;
+// let HEIGHT= document.documentElement.clientHeight;
+
+// canvas.width= WIDTH;
+// canvas.height= HEIGHT;
+
+// function random(number) {
+//     return Math.floor(Math.random()*number);
+// }
+
+// function draw() {
+//     ctx.clearRect(0, 0, WIDTH, HEIGHT);
+//     for(let i = 0; i < 100; i++) {
+//         ctx.beginPath();
+//         ctx.fillStyle= 'rgba(255, 0, 0, 0.5)';
+//         ctx.arc(random(WIDTH), random(HEIGHT), random(75), 0, 2 * Math.PI);
+//         ctx.fill();
+//     }
+// }
+//     btn.addEventListener('click', draw);
+
